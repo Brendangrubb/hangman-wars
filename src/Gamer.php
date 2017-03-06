6ab3428bcd42fcb268043b45876cbceb3895825d
Erica Wright and Zach Swanson <erica+zach@>
2017-03-06 11:55:54 -0800

    copy project from Zach's weekend testing

---
+++ src/Gamer.php
@@ -0,0 +1,18 @@
+<?php
+    class Gamer
+    {
+        public $color1;
+        public $color2;
+        public $color3;
+
+        function __construct()
+        {
+            $this->color1 = "red";
+            $this->color2 = "blue";
+            $this->color3 = "yellow";
+
+        }
+    }
+
+
+?>
