363059bff6760813466edd35c5ecd07472bda135
Erica Wright and Zach Swanson <erica+zach@>
2017-03-07 11:49:26 -0800

    add method to prepare phrase from database for gameplay

--- src/Phase.php
+++ src/Phase.php
@@ -6,7 +6,7 @@ class Phase
     private $phrase;
     private $score;
     private $roundIndex;
-    private $currentword;
+    private $currentWord;
     private $availableLetters;
     private $guessLetters;
     private $displayLetters;
@@ -17,42 +17,14 @@ class Phase
     private $phrasePositions;
     private $displayPhrase;
 
-
-    /*
-      Class Behaviors:
-      database holds quote phrases as strings
-
-      retreive the correct phrase from database
-
-      set empty initializaiton values: roundindex = 0, guessletters, available letters, and displayletters are empty arrays, currentword = "",
-
-      initialize score
-
-      split phrase into array of individual words (explode by " ", return array of strings)
-      count number of words in phrase (numberOfWords = phrase.length)
-
-      for each word in phrase:
-      create property of this.("word" . 1-index of loop) = str_split of the word.
-      push 1-index integer into roundOrder array
-      push word.length number of underscores into displayPhrase array
-      Initialize phrase positions with number of dummy characters equal to phrase length
-      add values to phrasePositions:
-        loop over phrase array
-            if word == word in array
-                write word number into phrase positions at index of found word
-
-
-
-      randomize order of roundOrder
-    */
-
     function __construct()
     {
+        // set empty initializaiton values: roundindex = 0, guessletters, available letters, and displayletters are empty arrays, currentword = "",
         $this->availableLetters = [];
         $this->displayLetters = [];
         $this->guessLetters = [];
         $this->roundIndex = 0;
-        $this->currentword = "";
+        $this->currentWord = "";
     }
 
     function getPhrase()
@@ -63,11 +35,13 @@ class Phase
 
     function setScore()
     {
+        // initialize score
         $this->score = 40;
     }
 
     function setPhrase($search_id)
     {
+        // retreive the correct phrase from database
         $query = $GLOBALS['DB']->query("SELECT * FROM phrases WHERE id = {$search_id}");
         $result = $query->fetch();
         $this->phrase = $result['phrase'];
@@ -75,7 +49,36 @@ class Phase
 
     function setValues()
     {
+        // split phrase into array of individual words (explode by " ", return array of strings)
+        $split_phrase = explode(' ', $this->phrase);
+        //count number of words in phrase (numberOfWords = phrase.length)
+        $this->numberOfWords = count($split_phrase);
+        //Initialize phrase positions with number of dummy characters equal to phrase length
+        $this->phrasePositions = array_fill(0, count($split_phrase), "_");
+        //for each word in phrase:
+        foreach ($split_phrase as $key => $word)
+        {
+            //create property of this.("word" . 1-index of loop) = str_split of the word.
+            $this->{"word" . ($key + 1)} = explode('', $word);
+            //push 1-index integer into roundOrder array
+            array_push($this->$roundOrder, ($key + 1));
+            //push word.length number of underscores into displayPhrase array
+            $displayPhrase = array_fill(0, strlen($word), "_");
+            array_push($this->displayPhrase, implode('', $displayPhrase);
+            //add values to phrasePositions:
+            //loop over phrase array
+            foreach ($split_phrase as $index => $replace_word)
+            {
+                //  if word == word in array
+                //  write word number into phrase positions at index of found word
+                if ($replace_word == $word) {
+                    array_splice($this->phrasePositions, $index, 1, ($key + 1);
+                }
+            }
 
+        }
+        //randomize order of roundOrder */
+        shuffle($this->roundOrder);
     }
 
 }
