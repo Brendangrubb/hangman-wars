975cd9af569da1add1d6a8a24ab9748fcf60e751
brendan <brendangrubb@gmail.com>
2017-03-08 12:04:02 -0800

    fix jquery interval times, add beginnings of keyframe color to text

--- web/js/win-scripts.js
+++ web/js/win-scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     // $('#final1').fadeIn(500);
     setInterval(function(){ $('#final2').fadeIn(1000); }, 2500);
-    setInterval(function(){ $('#final3').fadeIn(1000); }, 5500);
-    setInterval(function(){ $('#final4').fadeIn(1000); }, 9000);
-    setInterval(function(){ $('#final5').fadeIn(1000); }, 12500);
-    setInterval(function(){ $('#final6').fadeIn(1000); }, 15500);
+    setInterval(function(){ $('#final3').fadeIn(1000); }, 6000);
+    setInterval(function(){ $('#final4').fadeIn(1000); }, 9500);
+    setInterval(function(){ $('#final5').fadeIn(1000); }, 13000);
+    setInterval(function(){ $('#final6').fadeIn(1000); }, 16000);
 
 });
