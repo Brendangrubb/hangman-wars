7fda87f5b0daaf0cf1e94df8ef1576926a934caa
Brendan Grubb and Zach Swanson <brendan+zach@>
2017-03-08 09:19:19 -0800

    add testing and functionality for CRUD methods for GameState class

--- src/GameState.php
+++ src/GameState.php
@@ -31,6 +31,31 @@
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
+        function updateProperty($property, $value)
+        {
+            $GLOBALS['DB']->exec("UPDATE game_state SET {$property} = '{$value}' WHERE id = {$this->id};");
+            $this->{$property} = $value;
+        }
+
+        static function getAll()
+        {
+            $found = $GLOBALS['DB']->query("SELECT * FROM game_state;");
+            $game_state = [];
+            foreach ($found as $game)
+            {
+                $current_score = $game['current_score'];
+                $realm_one = ($game['realm_one']==1);
+                $realm_two = ($game['realm_two']==1);
+                $realm_three = ($game['realm_three']==1);
+                $realm_four = ($game['realm_four']==1);
+                $realm_five = ($game['realm_five']==1);
+                $id = $game['id'];
+                $new_game_state = new GameState($current_score, $realm_one, $realm_two, $realm_three, $realm_four, $realm_five, $id);
+                array_push($game_state, $new_game_state);
+            }
+            return $game_state;
+        }
+
         static function find($search_id)
         {
             $found = $GLOBALS['DB']->query("SELECT * FROM game_state WHERE id = {$search_id};");
@@ -42,11 +67,16 @@
                 $realm_two = ($game['realm_two']==1);
                 $realm_three = ($game['realm_three']==1);
                 $realm_four = ($game['realm_four']==1);
-                $realm_five = ($game['realm_five']==1);            
+                $realm_five = ($game['realm_five']==1);
                 $id = $game['id'];
                 $game_state = new GameState($current_score, $realm_one, $realm_two, $realm_three, $realm_four, $realm_five, $id);
             }
             return $game_state;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM game_state;");
         }
 
 
--- tests/GameStateTest.php
+++ tests/GameStateTest.php
@@ -14,18 +14,64 @@
 
     class GameStateTest extends PHPUnit_Framework_TestCase
     {
+        protected function teardown()
+        {
+            GameState::deleteAll();
+        }
+
         function test_find()
         {
             // Arrange
             $new_game_state = new GameState();
             $new_game_state->save();
-            var_dump($new_game_state);
-
 
             // Act
             $result = GameState::find($new_game_state->id);
+
             // Assert
             $this->assertEquals($new_game_state, $result);
+        }
+
+        function test_deleteAll()
+        {
+            // Arrange
+            $new_game_state = new GameState();
+            $new_game_state->save();
+
+            // Act
+            GameState::deleteAll();
+            $result = GameState::getAll();
+
+            // Assert
+            $this->assertEquals([], $result);
+        }
+
+        function test_getAll()
+        {
+            // Arrange
+            $new_game_state = new GameState();
+            $new_game_state->save();
+
+            // Act
+            $result = GameState::getAll();
+
+            // Assert
+            $this->assertEquals([$new_game_state], $result);
+        }
+
+        function test_update()
+        {
+            // Arrange
+            $new_game_state = new GameState();
+            $new_game_state->save();
+            $new_game_state->updateProperty("realm_three", true);
+
+            // Act
+            $game_state = GameState::find($new_game_state->id);
+            $result = $game_state->realm_three;
+
+            // Assert
+            $this->assertEquals(true, $result);
         }
 
 
