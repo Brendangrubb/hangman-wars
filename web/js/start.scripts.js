dafd5f57b393c6cac5b19cb974e4048e6c8694e3
brendan grubb <brendangrubb@gmail.com>
2017-03-09 11:02:05 -0800

    establish show hide divs for tutorial

---
+++ web/js/start.scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    // $('#final1').fadeIn(500);
+    // $('#final1b').fadeIn(500);
+    setTimeout(function(){ $('#setup').fadeOut(1000); }, 6000);
+    setTimeout(function(){ $('#inst1').fadeIn(1000); }, 7000);
+    setTimeout(function(){ $('#inst1').fadeOut(1000); }, 10000);
+    setTimeout(function(){ $('#inst2').fadeIn(1000); }, 11000);
+    setTimeout(function(){ $('#inst2').fadeOut(1000); }, 14000);
+    setTimeout(function(){ $('#inst3').fadeIn(1000); }, 15000);
+    setTimeout(function(){ $('#inst3').fadeOut(1000); }, 18000);
+    setTimeout(function(){ $('#inst4').fadeIn(1000); }, 19000);
+    setTimeout(function(){ $('#inst4').fadeOut(1000); }, 23000);
+    setTimeout(function(){ $('#inst1').fadeIn(1000); }, 24000);
+    setTimeout(function(){ $('#inst1').fadeOut(1000); }, 27000);
+    setTimeout(function(){ $('#inst2').fadeIn(1000); }, 28000);
+    setTimeout(function(){ $('#inst2').fadeOut(1000); }, 31000);
+    setTimeout(function(){ $('#inst3').fadeIn(1000); }, 32000);
+    setTimeout(function(){ $('#inst3').fadeOut(1000); }, 35000);
+    setTimeout(function(){ $('#inst4').fadeIn(1000); }, 36000);
+    setTimeout(function(){ $('#inst4').fadeOut(1000); }, 40000);
+    setTimeout(function(){ $('#inst1').fadeIn(1000); }, 41000);
+    setTimeout(function(){ $('#inst1').fadeOut(1000); }, 44000);
+    setTimeout(function(){ $('#inst2').fadeIn(1000); }, 45000);
+    setTimeout(function(){ $('#inst2').fadeOut(1000); }, 48000);
+    setTimeout(function(){ $('#inst3').fadeIn(1000); }, 49000);
+    setTimeout(function(){ $('#inst3').fadeOut(1000); }, 52000);
+    setTimeout(function(){ $('#inst4').fadeIn(1000); }, 53000);
+
+
+});
