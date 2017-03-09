ad2ab1a25d802a9f61efa97b5ce1528d300f4eb5
brendan grubb <brendangrubb@gmail.com>
2017-03-09 09:21:41 -0800

    add styling, erica new gifs

--- web/js/win-scripts.js
+++ web/js/win-scripts.js
@@ -1,16 +1,17 @@
-$(document).ready(function () {
-    // $('#final1').fadeIn(500);
-    setInterval(function(){ $('#final2').fadeIn(1000); }, 2500);
-    setInterval(function(){ $('#final3').fadeIn(1000); }, 6000);
-    setInterval(function(){ $('#final4').fadeIn(1000); }, 9500);
-    setInterval(function(){ $('#final5').fadeIn(1000); }, 13000);
-    setInterval(function(){ $('#final6').fadeIn(1000); }, 16000);
-
-
-    setInterval(function(){ $('.final-win-div').fadeOut(1000); }, 22000);
-
-    setInterval(function(){ $('.final-win-div2').fadeIn(1000); }, 23000);
-    setInterval(function(){ $('.final-logo').fadeIn(1000); }, 26000);
-    setInterval(function(){ $('.new-game-button').fadeIn(1000); }, 28000);
-
-});
+// $(document).ready(function () {
+//     // $('#final1').fadeIn(500);
+//     // $('#final1b').fadeIn(500);
+//     setInterval(function(){ $('#final2').fadeIn(1000); }, 2500);
+//     setInterval(function(){ $('#final3').fadeIn(1000); }, 6000);
+//     setInterval(function(){ $('#final4').fadeIn(1000); }, 9500);
+//     setInterval(function(){ $('#final5').fadeIn(1000); }, 13000);
+//     setInterval(function(){ $('#final6').fadeIn(1000); }, 16000);
+//
+//
+//     setInterval(function(){ $('.final-win-div').fadeOut(1000); }, 22000);
+//
+//     setInterval(function(){ $('.final-win-div2').fadeIn(1000); }, 23000);
+//     setInterval(function(){ $('.final-logo').fadeIn(1000); }, 26000);
+//     setInterval(function(){ $('.new-game-button').fadeIn(1000); }, 28000);
+//
+// });
